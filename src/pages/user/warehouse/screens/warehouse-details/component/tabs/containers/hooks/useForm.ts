9f3895e7eb94ea31727a7743e49937ec147dcbd6
Aasiya <aasiyaafroz97@gmail.com>
2023-03-08 11:08:53 +0530

    backup

--- src/pages/user/warehouse/screens/warehouse-details/component/tabs/containers/hooks/useForm.ts
+++ src/pages/user/warehouse/screens/warehouse-details/component/tabs/containers/hooks/useForm.ts
@@ -8,23 +8,31 @@ import { IPermissionRequest, IWarehouses } from "../query/useApiAction";
 
 const defaultValues: IWarehouses = {
   id: 0,
-  warehouseName:"",
-  label:"",
-  email:"",
-  phoneNumber:0,
-  address:"",
-  country:"",
-  city:"",
-  zipcode:0,
-  longitude:0,
-  latitude:0,
-  status:"",
-  pickingStrategy:"",
-  receivingStrategy:"",
-  timezone:"",
-  receivingType:"",
-  defaultWarehouse:false,
-  allowPartialPicking:false
+  warehouseName: "",
+  locations: "",
+  parents: "",
+  quantity: 0,
+  containers_type: "",
+  height: 0,
+  width: 0,
+  max: 0,
+  volume: 0,
+  label: "",
+  email: "",
+  phoneNumber: 0,
+  address: "",
+  country: "",
+  city: "",
+  zipcode: 0,
+  longitude: 0,
+  latitude: 0,
+  status: "",
+  pickingStrategy: "",
+  receivingStrategy: "",
+  timezone: "",
+  receivingType: "",
+  defaultWarehouse: false,
+  allowPartialPicking: false,
 };
 
 const useWarehouseForm = (
@@ -35,30 +43,34 @@ const useWarehouseForm = (
   initialValues: IWarehouses = defaultValues,
 ) => {
   let validation: any = null;
-    validation = {
-      warehouseName: Yup.string().required(ErrorMessages.warehouse.warehouseName),
-      label: Yup.string().required(
-        ErrorMessages.warehouse.label,
-      ),
-      email: Yup.string().required(
-        ErrorMessages.warehouse.email,
-      ),
-      phoneNumber: Yup.string().required(ErrorMessages.warehouse.phoneNumber),
-      // .matches(URL_REGEX, ErrorMessages.screens.invalid_url),
-      address: Yup.string().required(ErrorMessages.warehouse.address),
-      country: Yup.string().required(ErrorMessages.warehouse.country),
-      city: Yup.string().required(ErrorMessages.warehouse.city),
-      zipcode: Yup.string().required(ErrorMessages.warehouse.zipCode),
-      longitude: Yup.string().required(ErrorMessages.warehouse.longitude),
-      latitude: Yup.string().required(ErrorMessages.warehouse.latitude),
-      status: Yup.string().required(ErrorMessages.warehouse.status),
-      pickingStrategy: Yup.string().required(ErrorMessages.warehouse.pickingStrategy),
-      receivingStrategy: Yup.string().required(ErrorMessages.warehouse.receivingStrategy),
-      receivingType: Yup.string().required(ErrorMessages.warehouse.receivingType),
-      defaultWarehouse: Yup.string().required(ErrorMessages.warehouse.defaultWarehouse),
-      allowPartialPicking: Yup.string().required(ErrorMessages.warehouse.allowPartialPicking),
-    };
-   
+  validation = {
+    warehouseName: Yup.string().required(ErrorMessages.warehouse.warehouseName),
+    label: Yup.string().required(ErrorMessages.warehouse.label),
+    email: Yup.string().required(ErrorMessages.warehouse.email),
+    phoneNumber: Yup.string().required(ErrorMessages.warehouse.phoneNumber),
+    // .matches(URL_REGEX, ErrorMessages.screens.invalid_url),
+    address: Yup.string().required(ErrorMessages.warehouse.address),
+    country: Yup.string().required(ErrorMessages.warehouse.country),
+    city: Yup.string().required(ErrorMessages.warehouse.city),
+    zipcode: Yup.string().required(ErrorMessages.warehouse.zipCode),
+    longitude: Yup.string().required(ErrorMessages.warehouse.longitude),
+    latitude: Yup.string().required(ErrorMessages.warehouse.latitude),
+    status: Yup.string().required(ErrorMessages.warehouse.status),
+    pickingStrategy: Yup.string().required(
+      ErrorMessages.warehouse.pickingStrategy,
+    ),
+    receivingStrategy: Yup.string().required(
+      ErrorMessages.warehouse.receivingStrategy,
+    ),
+    receivingType: Yup.string().required(ErrorMessages.warehouse.receivingType),
+    defaultWarehouse: Yup.string().required(
+      ErrorMessages.warehouse.defaultWarehouse,
+    ),
+    allowPartialPicking: Yup.string().required(
+      ErrorMessages.warehouse.allowPartialPicking,
+    ),
+  };
+
   const schema = Yup.object().shape(validation);
   return useFormik<IWarehouses>({
     initialValues,
