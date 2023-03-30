2f0a09ccce6ed1d3786d0588097f33efa0d1d961
nadeem.softw <nadeem.softw@gmail.com>
2023-03-30 15:57:00 +0530

    Receive

--- src/pages/user/stock-control/recieve/Recieve.tsx
+++ src/pages/user/stock-control/recieve/Recieve.tsx
@@ -1,7 +1,45 @@
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { Box, CardContent, Container } from "@mui/material";
+// import { Container, Box } from "@mui/system";
+import TableToolbar from "components/table-toolbar";
 import RecieveList from "./component/list/RecieveList";
 
 function Recieve() {
-  return <RecieveList />;
+  return (
+    <Container maxWidth={false}>
+      <CardContent sx={{ paddingTop: 0 }}>
+        <TableToolbar
+          hasBulk
+          isBulkDisabled={false}
+          navTitle="Stock Control"
+          rightActions={[
+            {
+              id: crypto.randomUUID(),
+              title: "New",
+              onClick: () =>
+                // navigate(
+                //   `/${AppRoutes.stockControl.layout}/${AppRoutes.stockControl.adjustment.create}`,
+                // ),
+                {},
+              icon: (
+                <AddCircleIcon
+                  sx={{
+                    fontSize: 18,
+                    mr: 1,
+                  }}
+                />
+              ),
+            },
+          ]}
+          title="Receive"
+          onBulkHandle={() => {}}
+        />
+        <Box sx={{ mt: 3 }}>
+          <RecieveList />
+        </Box>
+      </CardContent>
+    </Container>
+  );
 }
 
 export default Recieve;
--- src/pages/user/stock-control/recieve/component/list/RecieveList.tsx
+++ src/pages/user/stock-control/recieve/component/list/RecieveList.tsx
@@ -1,7 +1,140 @@
-import RecieveListItem from "./RecieveListItem";
+import {
+  Box,
+  Checkbox,
+  Paper,
+  Table,
+  TableBody,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import CustomTableCell from "components/table/CustomTableCell";
+import EnhancedTableToolbar from "components/table/enhanced-table-toolbar";
+import PerfectScrollbar from "react-perfect-scrollbar";
+import "react-perfect-scrollbar/dist/css/styles.css";
+import ReceiveListItem from "./RecieveListItem";
+
+const tableTitle = [
+  {
+    id: crypto.randomUUID(),
+    title: "ID #",
+  },
+
+  {
+    id: crypto.randomUUID(),
+    title: "Received/Expected",
+  },
+
+  {
+    id: crypto.randomUUID(),
+    title: "Receiving Warehouse",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Client Company",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "From",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Status",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Notes",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Tags",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Expected Date",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Last Update",
+  },
+];
+const tableTabs = [
+  {
+    id: crypto.randomUUID(),
+    title: "NEW",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "COMPLETED",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "IN PROGRESS",
+  },
+];
 
 function RecieveList() {
-  return <RecieveListItem />;
+  return (
+    <PerfectScrollbar>
+      <EnhancedTableToolbar tabs={tableTabs} />
+
+      <Box sx={{ minWidth: 1050, minHeight: 500 }}>
+        <TableContainer component={Paper}>
+          <PerfectScrollbar>
+            <Table
+              sx={{
+                height: "100%",
+              }}
+            >
+              <TableHead>
+                <TableRow>
+                  <CustomTableCell
+                    isCheck
+                    isHeader
+                    isSticky
+                    customStyle={{
+                      zIndex: 999,
+                    }}
+                    leftValue={0}
+                    minWt={50}
+                  >
+                    <Checkbox
+                      // checked={}
+                      color="primary"
+                      // onChange={}
+                    />
+                  </CustomTableCell>
+                  {tableTitle.map((item) => {
+                    const isSA = item.title.includes("ID #");
+
+                    return (
+                      <CustomTableCell
+                        key={item.id}
+                        isHeader
+                        customStyle={{
+                          position: isSA ? "sticky" : "static",
+                          left: isSA ? 50 : 0,
+                        }}
+                        minWt={170}
+                      >
+                        {item.title}
+                      </CustomTableCell>
+                    );
+                  })}
+                  <CustomTableCell isHeader isSticky rightValue={0}>
+                    Actions
+                  </CustomTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <ReceiveListItem item={undefined} />
+              </TableBody>
+            </Table>
+          </PerfectScrollbar>
+        </TableContainer>
+      </Box>
+    </PerfectScrollbar>
+  );
 }
 
 export default RecieveList;
