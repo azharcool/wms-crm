223a45c16cf1789524841900a1a8b50148448587
nadeen <nadeem.softw@gmail.com>
2023-04-11 13:51:13 +0530

    stock-count1

--- src/pages/user/stock-control/stock-count/StockCount.tsx
+++ src/pages/user/stock-control/stock-count/StockCount.tsx
@@ -1,10 +1,43 @@
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { Box, CardContent, Container } from "@mui/material";
+import TableToolbar from "components/table-toolbar";
 import StockCountList from "./component/list/StockCountList";
 
 function StockCount() {
   return (
-    <>
-      <StockCountList />
-    </>
+    <Container maxWidth={false}>
+      <CardContent sx={{ paddingTop: 0 }}>
+        <TableToolbar
+          hasBulk
+          isBulkDisabled={false}
+          navTitle="Stock Control"
+          rightActions={[
+            {
+              id: crypto.randomUUID(),
+              title: "New",
+              onClick: () =>
+                // navigate(
+                //   `/${AppRoutes.stockControl.layout}/${AppRoutes.stockControl.adjustment.create}`,
+                // ),
+                {},
+              icon: (
+                <AddCircleIcon
+                  sx={{
+                    fontSize: 18,
+                    mr: 1,
+                  }}
+                />
+              ),
+            },
+          ]}
+          title="Stock Count"
+          onBulkHandle={() => {}}
+        />
+        <Box sx={{ mt: 3 }}>
+          <StockCountList />
+        </Box>
+      </CardContent>
+    </Container>
   );
 }
 
